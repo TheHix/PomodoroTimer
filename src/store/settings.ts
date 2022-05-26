bda7fd1eeb1f254b143ed5b2f84d626967f2021a
Ssaveliy <saveliy-sokolnikov@mail.ru>
2022-05-26 23:00:26 +0300

    a class was created to work with the state through mobX

---
+++ src/store/settings.ts
@@ -0,0 +1,19 @@
+import { makeAutoObservable, makeObservable } from "mobx";
+import { storage } from "../tools/storage";
+
+class Settings {
+    settingsInfo = {
+        breakMinutes: storage.getInfoBreakMinutes() ?? 5,
+        workMinutes: storage.getInfoWorkMinutes() ?? 25,
+    };
+    constructor() {
+        makeAutoObservable(this);
+    }
+    setWorkMinutes(value: number): void {
+        this.settingsInfo.workMinutes = value;
+    }
+    setBreakMinutes(value: number): void {
+        this.settingsInfo.breakMinutes = value;
+    }
+}
+export default new Settings();
